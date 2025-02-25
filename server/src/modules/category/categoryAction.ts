2508e1de28bc5f89a1ba3f313a4cb0a0973efda6
mitsuri91 <launaymaxime5@gmail.com>
2025-02-25 12:05:28 +0100

    add repo program

--- server/src/modules/category/categoryAction.ts
+++ server/src/modules/category/categoryAction.ts
@@ -1,3 +1,6 @@
+// Import access to data
+import categoryRepository from "./categoryRepository";
+
 // Some data to make the trick
 
 const categories = [
@@ -11,27 +14,23 @@ const categories = [
   },
 ];
 
+// Declare the actions
+
 import type { RequestHandler } from "express";
 
-const browse: RequestHandler = (req, res) => {
-  if (req.query.q != null) {
-    const filteredCategories = categories.filter((categorie) =>
-      categorie.name.includes(req.query.q as string),
-    );
+const browse: RequestHandler = async (req, res) => {
+  const categoriesFromDB = await categoryRepository.readAll();
 
-    res.json(filteredCategories);
-  } else {
-    res.json(categories);
-  }
+  res.json(categoriesFromDB);
 };
 
 const read: RequestHandler = (req, res) => {
   const parsedId = Number.parseInt(req.params.id);
 
-  const categorie = categories.find((p) => p.id === parsedId);
+  const category = categories.find((p) => p.id === parsedId);
 
-  if (categorie != null) {
-    res.json(categorie);
+  if (category != null) {
+    res.json(category);
   } else {
     res.sendStatus(404);
   }
